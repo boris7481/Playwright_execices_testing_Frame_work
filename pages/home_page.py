03a2e029bf7571a4a6dbbbd6a8344912c26a698a
Tondjua <boristondjua@gmail.com>
2026-04-04 14:29:47 +0200

    POM successful implemented prudeuct

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,14 +2,30 @@ from playwright.sync_api import expect
 
 
 class HomePage:
-# nouvelle modification de boris tacheuno
     def __init__(self, page):
         self.page = page
 
-    def navigate(self):
+    def navigate_without_login(self):
         self.page.goto("https://www.automationexercise.com/")
+        self.page.get_by_role("button", name="Einwilligen").click()
+        expect(self.page.get_by_text("Video Tutorials")).to_be_visible()
+
+
 
     def selectordernavigationlink(self):
-        self.page.get_by_role("button", name="Einwilligen").click()
+      #  self.page.get_by_role("button", name="Einwilligen").click()
         self.page.get_by_role("link", name="Signup / Login").click()
-        expect(self.page.get_by_text("Video Tutorials")).to_be_visible()
+      #  expect(self.page.get_by_text("Video Tutorials")).to_be_visible()
+
+    def view_women_items(self):
+        self.page.get_by_role("link", name="Women").click()
+        self.page.get_by_role("link", name="Dress").click()
+
+    def view_men_items(self):
+        self.page.get_by_role("link", name="Men").click()
+        self.page.get_by_role("link", name="Tshirts").click()
+
+    def navigate_and_click_of_product_link(self):
+        self.page.get_by_role("link", name=" Products").click()
+
+
--- pages/login_page.py
+++ pages/login_page.py
@@ -15,3 +15,5 @@ class LoginPage:
         self.page.locator('[data-qa="login-button"]').click()
     #    expect(self.page.get_by_text("Login to your account")).to_be_visible()
     #    expect(self.page.get_by_text("Your email or password is incorrect!")).to_be_visible()
+
+
